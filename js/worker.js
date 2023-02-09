21757421ff86a7d40b32b611fed5673bed831d83
Gopi Krishna Suvanam <apple@Gopis-MacBook-Air.local>
2023-02-09 17:00:30 +0530

    last line display

--- js/worker.js
+++ js/worker.js
@@ -1,6 +1,5 @@
 ﻿
 run=function(_block_id){
-	console.log("Running cell ",_block_id)
 	
 	/*var show =function(x){
 		show_in_dom(x,"output"+_block_id)
@@ -8,7 +7,6 @@ run=function(_block_id){
 	curr_cell=function(){
 		return get_dom("output"+_block_id);
 	}
-	console.log("hi ",get_dom("cell_type"+_block_id).checked)
 	get_dom("status"+_block_id).innerHTML='[*]'
 	get_dom("output"+_block_id).innerHTML=''
 	
@@ -24,8 +22,23 @@ run=function(_block_id){
 			get_dom("output"+_block_id).style.display="block";
 			get_dom("input"+_block_id).style.display = "block";
 			const start_time_eval = Date.now();
-			show_string=`show=(...args)=>show_in_dom("output"+${_block_id},...args);`;
-			Function(show_string+code)(); // This is where the magic happens.
+			show=(...args)=>show_in_dom(`output${_block_id}`,...args);
+			opt=eval(code); // This is where the magic happens.
+			try{
+				//This is for displaying the last line by default.
+				//The line will be displayed only if it does not have an assignment or a function call.
+				//These are excluded to avoid side-effects.
+				/*var last_line=code.trim().split(/\r?\n/).pop().replace(/;$/,"").split(";").pop();
+				if(!last_line.replace("===","").replace("==","").includes("=") && !last_line.includes("("))
+					eval(`show(${last_line})`);
+				*/
+				
+				//We will use the output of eval to display in stead.. we will use the above code in case we ditch eval in some future time.
+				if(opt!=undefined) show(opt);
+				
+			}catch(err){
+				console.log(err)
+			}
 			const end_time_eval = Date.now();
 			var execution_time=end_time_eval - start_time_eval;
 			
@@ -48,7 +61,6 @@ run=function(_block_id){
 		get_dom("output"+_block_id).innerHTML=get_dom("output"+_block_id).innerHTML+"<p class='error'>"+err.message+"</p>";
 		get_dom("status"+_block_id).innerHTML='[-]'
 	}
-	console.log("Finished cell ",_block_id)
 }
 
 run_all=function(){
