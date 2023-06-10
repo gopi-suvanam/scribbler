6b73433b55381be9c8a8b52af785e4abf9545510
Gopi Krishna Suvanam <apple@Gopis-MacBook-Air.local>
2023-06-10 11:30:09 +0530

    sitemap

--- sitemap.sh
+++ sitemap.sh
@@ -6,16 +6,19 @@ DIRECTORY="examples"
 # Output file path for the sitemap
 SITEMAP_FILE="sitemap.xml"
 
+# Main site lin
+SITE_LOCATION="https://decentralized-intelligence.com/jsnb"
+
 # Create the XML sitemap header
 echo '<?xml version="1.0" encoding="UTF-8"?>' > "$SITEMAP_FILE"
 echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' >> "$SITEMAP_FILE"
 echo '  <url>' >> "$SITEMAP_FILE"
-echo "    <loc>https://decentralized-intelligence.com/jsnb</loc>" >> "$SITEMAP_FILE"
+echo "    <loc>$SITE_LOCATION</loc>" >> "$SITEMAP_FILE"
 echo "    <lastmod>2023-04-28T00:00:00+00:00</lastmod>" >> "$SITEMAP_FILE"
 echo '  </url>' >> "$SITEMAP_FILE"
    
 echo '  <url>' >> "$SITEMAP_FILE"
-echo "    <loc>https://decentralized-intelligence.com/jsnb/examples/README</loc>" >> "$SITEMAP_FILE"
+echo "    <loc>$SITE_LOCATION/examples/README</loc>" >> "$SITEMAP_FILE"
 echo "    <lastmod>$(date +'%Y-%m-%dT%H:%M:%SZ')</lastmod>" >> "$SITEMAP_FILE"
 echo '  </url>' >> "$SITEMAP_FILE"
 
@@ -27,7 +30,7 @@ do
   FILE_MODIFIED=$(stat -f %m "$FILE")
   
   echo '  <url>' >> "$SITEMAP_FILE"
-  echo "    <loc>https://decentralized-intelligence.com/jsnb/#./examples/$FILENAME_ESCAPED</loc>" >> "$SITEMAP_FILE"
+  echo "    <loc>$SITE_LOCATION/#./examples/$FILENAME_ESCAPED</loc>" >> "$SITEMAP_FILE"
   echo "    <lastmod>$(date -r $FILE_MODIFIED +'%Y-%m-%dT%H:%M:%SZ')</lastmod>" >> "$SITEMAP_FILE"
   echo '  </url>' >> "$SITEMAP_FILE"
 done
