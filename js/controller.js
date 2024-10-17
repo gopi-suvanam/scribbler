5ecdd93d9f467002e85bed62535e928a772cad31
Gopi Krishna Suvanam <gopi.suvanam@gmail.com>
2024-10-17 14:28:17 +0530

    sahreable Link clean

--- js/controller.js
+++ js/controller.js
@@ -329,7 +329,7 @@ loadLocalFile=function(id){
 shareBtn=function(){
  	let url='';
  	scrib.getDom("sharableLink").innerHTML= window.location;//.origin+window.location.pathname+'?jsnb='+url;
-  			
+  	
 	try{ 
 		const urlParams = new URLSearchParams(window.location.search);
 		const jsnb_path = urlParams.get('jsnb');
@@ -339,6 +339,9 @@ shareBtn=function(){
 		url='';
 	}
 	if(url==undefined) url='';
+	scrib.getDom("sharableLinkClean").innerHTML= window.origin+window.location.pathname+`?jsnb=${url}&hide-menu=true&hide-code=true`;
+  	
+  	
 	if(url.length>0)	
 		scrib.getDom("iframeLink").innerText='<iframe id="sandbox" style="width:100%;height:100%" src ="'+window.location.origin+window.location.pathname+'sandbox.html?jsnb='+url+'"></iframe>';
   	else alert("Push the notebook to Github first to publish the notebook in an iFrame");
