152331b9f3ba06b5273de02248266bffbb69a88f
Gopi Krishna Suvanam <gopi.suvanam@gmail.com>
2025-01-28 10:47:53 +0530

    changes

--- js/api-wrapper.js
+++ js/api-wrapper.js
@@ -27,8 +27,19 @@ APIWrapper.call = async function(source,key,params){
 		else return result.candidates[0].content.parts.map(part=>part.text).join("\n");	
 	}
 	if(source=='OpenVino'){
-		const url = `http://localhost:5000/generate?prompt=${params.prompt}`;
-		const response = await fetch(url);
+		const url = 'http://localhost:5000/generate';
+
+
+		
+		// Make the POST request
+		const response = await fetch(url, {
+		    method: 'POST', // Use POST instead of GET
+		    headers: {
+		        'Content-Type': 'application/json' // Set the content type to JSON
+		    },
+		    body: JSON.stringify(params) // Convert the payload to JSON
+		})
+
 		const result= await response.json();
 		if("error" in result) throw "No reponse";
 		else return result[0]['generated_text'];
