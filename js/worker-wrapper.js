ffc4846908e30f5ddd52381afa2d07ebb7440348
Jaimin <jaiminkaneriya@gmail.com>
2025-09-27 13:32:13 +0530

    refactor: move SVG icons to sprite file and update references

--- js/worker-wrapper.js
+++ js/worker-wrapper.js
@@ -146,7 +146,7 @@ worker.run= async function(_block_id){
 	scrib.getDom("status"+_block_id).innerHTML='[*]'
 	scrib.getDom("output"+_block_id).innerHTML=''
 	
-	scrib.getDom("run-button"+_block_id).innerHTML = `<svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="animation: spin 1s linear infinite;"><line x1="12" y1="2" x2="12" y2="6"></line><line x1="12" y1="18" x2="12" y2="22"></line><line x1="4.93" y1="4.93" x2="7.76" y2="7.76"></line><line x1="16.24" y1="16.24" x2="19.07" y2="19.07"></line><line x1="2" y1="12" x2="6" y2="12"></line><line x1="18" y1="12" x2="22" y2="12"></line><line x1="4.93" y1="19.07" x2="7.76" y2="16.24"></line><line x1="16.24" y1="7.76" x2="19.07" y2="4.93"></line></svg>`;
+	scrib.getDom("run-button"+_block_id).innerHTML = `<svg width="16" height="16" style="animation: spin 1s linear infinite;"><use href="../images/icons-sprite.svg#icon-spinner"></use></svg>`;
 	
 	const code=sandbox.editors[_block_id].getValue()
 	
@@ -214,7 +214,7 @@ worker.run= async function(_block_id){
 		}
 		
 		scrib.getDom("run-button"+_block_id).setAttribute("data-tooltip","Finished running the cell");
-		scrib.getDom("run-button"+_block_id).innerHTML = `<svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3"></polygon></svg>`;
+		scrib.getDom("run-button"+_block_id).innerHTML = `<svg width="16" height="16"><use href="../images/icons-sprite.svg#icon-run-button"></use></svg>`;
 		setTimeout(()=>{
 			scrib.getDom("run-button"+_block_id).setAttribute("data-tooltip","Run again");
 			}
