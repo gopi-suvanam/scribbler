13d18df8757b47d28de4d86e3eb9cc559f59af3e
gopi <gopi@scribbler.live>
2025-10-06 16:06:29 +0530

    run button

--- js/worker-wrapper.js
+++ js/worker-wrapper.js
@@ -146,7 +146,7 @@ worker.run= async function(_block_id){
 	scrib.getDom("status"+_block_id).innerHTML='[*]'
 	scrib.getDom("output"+_block_id).innerHTML=''
 	
-	scrib.getDom("run-button"+_block_id).innerHTML = `<svg width="16" height="16" style="animation: spin 1s linear infinite;"><use href="../images/icons-sprite.svg#icon-spinner"></use></svg>`;
+	scrib.getDom("run-button"+_block_id).innerHTML = `<svg width="16" height="16" style="animation: spin 1s linear infinite;"><use href="icon-spinner"></use></svg>`;
 	
 	const code=sandbox.editors[_block_id].getValue()
 	
@@ -214,7 +214,7 @@ worker.run= async function(_block_id){
 		}
 		
 		scrib.getDom("run-button"+_block_id).setAttribute("data-tooltip","Finished running the cell");
-		scrib.getDom("run-button"+_block_id).innerHTML = `<svg width="16" height="16"><use href="../images/icons-sprite.svg#icon-run-button"></use></svg>`;
+		scrib.getDom("run-button"+_block_id).innerHTML = `<svg width="16" height="16"><use href="icon-run-button"></use></svg>`;
 		setTimeout(()=>{
 			scrib.getDom("run-button"+_block_id).setAttribute("data-tooltip","Run again");
 			}
